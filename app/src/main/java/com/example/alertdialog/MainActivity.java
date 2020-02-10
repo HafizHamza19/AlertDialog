956fe2c612c99007b72641fdd4d393cee9e8097c
HafizHamza19 <HafizHamza0322@gmail.com>
2020-02-10 20:07:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are You Sure?")
+                .setMessage("Do You Really Want to Do this?")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Thanks", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton("No",null).show();
+    }
+}
